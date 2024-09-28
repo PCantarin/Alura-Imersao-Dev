806b2d020c8ef81adc365a961440be62b708a195
PCantarin <pedrocantarin89@gmail.com>
2024-09-28 12:43:12 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,8 @@
+let mario = {
+    titulo: "Super Mario World",
+    descricao: "Super Mario World é um dos jogos de plataforma mais amados de todos os tempos,
+                    lançado originalmente para o Super Nintendo
+                    Entertainment System(SNES) em 1990. Nessa aventura épica, os encanadores mais famosos do mundo,
+    Mario e Luigi, são transportados
+                    para a mágica Dinosaur Land, um reino repleto de desafios, segredos e criaturas pré- históricas.",
+}
